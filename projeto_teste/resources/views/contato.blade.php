715d8301043a690f3ad594a266ecc3b6ea1cf2a6
erikolivera2099 <erikolivera2099@alu.ufc.br>
2021-06-04 15:57:30 -0300

    upload do projeto de laravel

---
+++ projeto_teste/resources/views/contato.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.main')
+
+@section('title', 'Produtos')
+
+@section('content')
+
+<h1>Esta é a página de contato</h1>
+<h2>(88) 9 9999-9999</h2>
+
+<a href="/">Voltar para página inicial</a>
+
+@endsection
---
+++ projeto_teste/resources/views/events/show.blade.php
@@ -0,0 +1,56 @@
+@extends('layouts.main')
+
+@section('title', $event->title)
+
+@section('content')
+
+<div class="col-md-10 offset-md-1">
+    <div class="row">
+        <div id="image-container" class="col-md-6">
+            @if($event->image != null)
+                <img src="/images/events/{{ $event->image }}" class="img-fluid" alt="{{ $event->title }}">
+            @else
+                <img src="/images/image_default.jpg" class="img-fluid" alt="{{ $event->title }}">
+            @endif
+        </div>
+        <div id="info-container" class="col-md-6">
+            <h1> {{ $event->title }} </h1>
+            <p class="event-city"><ion-icon name="location-outline"></ion-icon>{{ $event->city}}</p>
+            <p class="events-participants"><ion-icon name="people-outline"></ion-icon> 
+                @if(count($event->users) == 1)
+                    {{ count($event->users) }} Participante
+                @elseif(count($event->users) > 1)
+                    {{ count($event->users) }} Participantes
+                @elseif(count($event->users) == 0)
+                    Não há ninguém participando deste evento
+                @endif
+            </p>
+            <p class="event-owner"><ion-icon name="star-outline"></ion-icon> {{ $eventOwner['name'] }} </p>
+                @if(!$hasUserJoined)
+                <form action="/events/join/{{ $event->id }}" method="POST">
+                    @csrf
+                    <a href="/events/join/{{ $event->id }}" 
+                    class="btn btn-primary" 
+                    id="event-submit"
+                    onclick="event.preventDefault(); this.closest('form').submit();">
+                    Confirmar presença
+                    </a>
+                </form>
+                @else
+                    <p class="already-joined-msg">Você já está participando deste evento!</p>
+                @endif
+            <h3>O evento conta com:</h3>
+            <ul id="items-list">
+                @foreach($event->items as $item)
+                    <li> <ion-icon name="play-outline"></ion-icon> <span> {{ $item }} </span> </li>
+                @endforeach
+            </ul>
+        </div>
+        <div class="col-md-12" id="description-container">
+            <h3>Sobre o evento:</h3>
+            <p class="event-description"> {{ $event->description}} </p>
+        </div>
+    </div>
+</div>
+
+@endsection
